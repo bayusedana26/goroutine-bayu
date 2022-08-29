fbc1426741ad894dd6cb4e747cdbcff9085a6627
Bayu Sedana <bayusedana26@gmail.com>
2022-08-29 21:19:50 +0700

    atomic, cond, in out, select chan

--- channel_test.go
+++ channel_test.go
@@ -2,20 +2,153 @@ package goroutinebayu
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
 
 func TestCreateChannel(t *testing.T) {
 	channel := make(chan string)
+	defer close(channel)
 
 	go func() {
 		time.Sleep(2 * time.Second)
+		channel <- "Bayu sedana is here"
 		fmt.Println("Done sent data to Channel")
 
 	}()
 	data := <-channel
 	fmt.Println(data)
+
 	time.Sleep(5 * time.Second)
-	close(channel) // Also can use defer close
+}
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Response please"
+}
+
+func TestChannelParam(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Try only in"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Bayu"
+		channel <- "Sedana"
+		channel <- "Isdiyono"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Done")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke :" + strconv.Itoa(i)
+		}
+		defer close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Receiving", data)
+	}
+
+	fmt.Println("Done")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel 2", data)
+			counter++
+		default:
+			fmt.Println("Waiting to receive data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
--- treasure_test.go
+++ treasure_test.go
@@ -1,7 +1,8 @@
-package main
+package goroutinebayu
 
 import (
 	"fmt"
+	"testing"
 	"time"
 )
 
@@ -9,6 +10,15 @@ var channel = make(chan string)
 var storage = make(chan string)
 
 var items = [7]string{"Rock", "Treasure", "Coral", "Treasure", "Rock", "Treasure", "Coral"}
+
+func TestTreasure(t *testing.T) {
+
+	go getItem(items)
+	go cleanItem()
+	go savedItem()
+
+	time.Sleep(5 * time.Second)
+}
 
 // Running 3 goroutines, use channel to interact
 
@@ -24,7 +34,7 @@ func getItem(items [7]string) {
 func cleanItem() {
 	for rawItem := range channel {
 		fmt.Println("Success clean", rawItem)
-		storage <- "Clean Treasure is been moved"
+		storage <- "is safe now"
 	}
 }
 
@@ -32,13 +42,4 @@ func savedItem() {
 	for save := range storage {
 		fmt.Println("Success keep the treasure", save)
 	}
-}
-
-func main() {
-
-	go getItem(items)
-	go cleanItem()
-	go savedItem()
-
-	time.Sleep(5 * time.Second)
 }
